a2d6d24906f77ad231aa3532b20d11fc0d52abc5
iamenochchirima <iamenochchirima@gmail.com>
2023-02-23 23:58:46 +0200

    Configured analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,48 @@
 import '@/styles/globals.css'
-import Head from "next/head";
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import * as gtag from '../lib/gtag'
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
-        <title>Enoch Chirima</title>
+      <title>Enoch Chirima</title>
         <link rel="icon" href="/myicon..png" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <Component {...pageProps} />
     </>
   )
 }
+
+export default App
