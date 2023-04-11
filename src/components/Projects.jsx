2ca8e247e1b4717fb92c6050af58b5d45f11630f
iamenochchirima <iamenochchirima@gmail.com>
2023-04-11 20:42:55 +0200

    more projects link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -49,15 +49,15 @@ const Projects = () => {
                   </ul>
                 </div>
                 <div className="flex justify-center text-white space-x-10">
-                {project.codeLink ? (
+                  {project.codeLink ? (
                     <Link
                       href={project.codeLink}
                       target="_blank"
                       rel="noreferrer"
-                      className="hover:bg-gray-900 px-2 py-1 rounded"
+                      className="hover:bg-gray-900 px-2 py-1"
                     >
-                      <button className="rounded flex items-center">
-                      <FaCode /> <span className="ml-2">Code</span>
+                      <button className="hover:bg-gray-700 rounded-full p-2 flex items-center">
+                        <FaCode /> <span className="ml-2">Code</span>
                       </button>
                     </Link>
                   ) : (
@@ -73,9 +73,9 @@ const Projects = () => {
                       href={project.visitLink}
                       target="_blank"
                       rel="noreferrer"
-                      className="hover:bg-gray-900 px-2 py-1 rounded"
+                      className="hover:bg-gray-900 px-2 py-1"
                     >
-                      <button className="rounded flex items-center">
+                      <button className="hover:bg-gray-700 rounded-full p-2 flex items-center">
                         <FaEye /> <span className="ml-2">Visit Site</span>
                       </button>
                     </Link>
@@ -92,6 +92,17 @@ const Projects = () => {
             </div>
           );
         })}
+      </div>
+      <div className="">
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/iamenochchirima?tab=repositories"
+        >
+          <h1 className="text-center text-white">
+            <span className="bg-gray-900 hover:bg-gray-700 rounded-full p-2">See more projects on my Github Here</span>
+          </h1>
+        </Link>
       </div>
     </div>
   );
